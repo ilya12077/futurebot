e07ac92abe6abc0ceaa94e1473955544910937b9
ilya12077 <mr.ilya.1207@gmail.com>
2023-11-19 17:58:51 +0300

    init

---
+++ main.py
@@ -0,0 +1,112 @@
+from dotenv import load_dotenv, find_dotenv
+from flask import Flask, request
+from waitress import serve
+
+from tools import *
+
+app = Flask(__name__)
+max_duplicate_messages = 3
+load_dotenv(find_dotenv())
+url = os.environ.get('URL')
+future_group_id = int(os.environ.get('FUTURE_GROUP_ID'))
+if os.environ.get('AM_I_IN_A_DOCKER_CONTAINER', False):
+    path = '/etc/telegrambot/'
+else:
+    path = ''
+with open(f'{path}wordlist.txt', 'r', encoding='utf-8') as fl:
+    wordlist = fl.read().split()
+with open(f'{path}captcha_denied.txt', 'r', encoding='utf-8') as fl:
+    captcha_denied = fl.read().split()
+
+
+@app.route('/', methods=['GET', 'POST'])
+def firewall():
+    if request.method == "GET":
+        return 'I\'m working'
+    r = request.get_json()
+    print(r)
+    if 'callback_query' in r:
+        if r['callback_query']['message']['chat']['id'] == future_group_id:
+            if str(r['callback_query']['from']['id']) == r['callback_query']['message']['reply_markup']['inline_keyboard'][0][0]['callback_data']:
+                try:
+                    captcha_denied.remove(r['callback_query']['message']['reply_markup']['inline_keyboard'][0][0]['callback_data'])
+                    with open(f'{path}captcha_denied.txt', 'w', encoding='utf-8') as f:
+                        f.write(' '.join(captcha_denied))
+                except ValueError:
+                    pass
+                delete_message(future_group_id, r['callback_query']['message']['message_id'])
+            requests.post(url + f"answerCallbackQuery?callback_query_id={r['callback_query']['id']}")
+    elif 'message' in r:
+        chat_id = int(r['message']['chat']['id'])
+        if chat_id == future_group_id:
+            group_handler(r)
+        elif r['message']['chat']['type'] == 'private':
+            dm_handler(r)
+    return 'OK'
+
+
+def group_handler(r):
+    user_id = str(r['message']['from']['id'])
+    message_id = r['message']['message_id']
+    chat_id = int(r['message']['chat']['id'])
+    first_name = r['message']['from']['first_name']
+    append_history(user_id, r)
+    if user_id in captcha_denied:
+        delete_message(chat_id, message_id)
+    if 'text' in r['message']:
+        msg = r['message']['text']
+        if count_duplicate_messages(user_id, message=msg) > max_duplicate_messages or any(word in msg.lower() for word in wordlist):
+            delete_message(chat_id, message_id)
+            append_log('', f'удалено сообщение от {first_name}({user_id}): {msg}')
+            return
+    elif 'sticker' in r['message']:
+        file_unique_id = r['message']['sticker']['thumbnail']['file_unique_id']
+        if count_duplicate_messages(user_id, file_unique_id=file_unique_id) > max_duplicate_messages:
+            delete_message(chat_id, message_id)
+            append_log('', f'удалено сообщение от {first_name}({user_id}): *sticker*')
+            return
+    elif 'left_chat_participant' in r['message']:
+        try:
+            captcha_denied.remove(str(r['message']['left_chat_participant']['id']))
+            with open(f'{path}captcha_denied.txt', 'w', encoding='utf-8') as f:
+                f.write(' '.join(captcha_denied))
+        except ValueError:
+            pass
+    elif 'new_chat_participant' in r['message']:
+        if 'username' in r['message']['from']:
+            username = '@' + r['message']['from']['username']
+        else:
+            username = first_name
+        send_message(chat_id, f'{username}, добро пожаловать в чатик! Нажимайте кнопку ниже, только если вы человек. Иначе вы не сможете писать в чат', {'inline_keyboard': [[{'text': 'Подтверждаю', 'callback_data': user_id}]]})
+        captcha_denied.append(str(r['message']['new_chat_participant']['id']))
+        with open(f'{path}captcha_denied.txt', 'w', encoding='utf-8') as f:
+            f.write(' '.join(captcha_denied))
+
+
+def dm_handler(r):
+    user_id = str(r['message']['from']['id'])
+    first_name = r['message']['from']['first_name']
+    if 'text' not in r['message']:
+        send_message(user_id, 'Я понимаю только текст')
+        return
+    msg = r['message']['text']
+    if user_id != '647372660':
+        append_log(first_name, msg)
+
+    match msg:
+        case '/start':
+            send_message(user_id, 'Чего желаешь? (я пока ничего не умею)')
+        case _:
+            send_message(user_id, 'Неизвестная команда')
+
+
+if __name__ == '__main__':
+    if os.environ.get('AM_I_IN_A_DOCKER_CONTAINER', False):
+        serve(app, host='0.0.0.0', port=8881, url_scheme='http')
+    else:
+        app.run(host='192.168.1.10', port=8881)
+        # app.run(host='192.168.1.21', port=8881)
+
+'''TODO:
+settings.*
+'''
---
+++ tools.py
@@ -0,0 +1,132 @@
+import datetime
+import html
+import json
+import os
+import time
+import ast
+
+import requests
+from dotenv import load_dotenv, find_dotenv
+
+safe_mode = False
+spam_timeout = 3 * 60  # в секундах
+load_dotenv(find_dotenv())
+url = os.environ.get('URL')
+if os.environ.get('AM_I_IN_A_DOCKER_CONTAINER', False):
+    path = '/etc/telegrambot/'
+else:
+    path = ''
+
+with open(f'{path}names.json', 'r') as fl:
+    ids = json.load(fl)
+
+
+def delete_message(chat_id, message_id):
+    requests.post(url + f'deleteMessage?chat_id={chat_id}&message_id={message_id}')
+
+
+def count_duplicate_messages(user_id: str, message: str = None, file_unique_id: str = None) -> int:
+    count = 0
+    with open(f'history.txt', 'r', encoding='utf-8') as f:
+        for i in f.readlines():
+            if i.split()[1] == user_id:
+                r = ast.literal_eval(i[i.find('{'):])
+                if 'text' in r['message']:
+                    if message == r['message']['text']:
+                        count += 1
+                elif 'sticker' in r['message']:
+                    if file_unique_id == r['message']['sticker']['thumbnail']['file_unique_id']:
+                        count += 1
+    return count
+
+
+def clear_history():
+    with open(f'{path}history.txt', 'r', encoding='utf-8') as f:
+        a = f.readlines()
+        for index, record in enumerate(a):
+            if int(time.time()) - int(record.split()[0]) > spam_timeout:
+                a[index] = ''
+    with open(f'{path}history.txt', 'w', encoding='utf-8') as f:
+        f.write(''.join(a))
+
+
+def append_history(user_id: int | str, r: str, date=time.time):
+    clear_history()
+    with open(f'{path}history.txt', 'a', encoding='utf-8') as f:
+        try:
+            f.write(f'{int(date())} {user_id} {r} \n')
+        except Exception as e:
+            f.write(f'Exception {e}' + '\n')
+
+
+def send_message(chat_id: int | str, message, keyboard=None, spoiler=False):
+    if spoiler:
+        message = f'<tg-spoiler>{message}</tg-spoiler>'
+    if keyboard is None:
+        send_body = {
+            'chat_id': chat_id,
+            'text': message,
+            'parse_mode': 'HTML'
+        }
+    else:
+        send_body = {
+            'chat_id': chat_id,
+            'text': message,
+            'parse_mode': 'HTML',
+            'reply_markup': keyboard
+        }
+    if safe_mode:
+        print(url + 'sendMessage', send_body)
+    else:
+        r = requests.post(url + 'sendMessage', json=send_body)
+        #print(r.content)
+        if r.status_code == 400:
+            send_message(chat_id, html.escape(message), keyboard, spoiler)
+
+
+def upload_photo(chat_id, file):
+    files = {
+        'photo': open(file, 'rb')
+    }
+    if safe_mode:
+        print(f'{url}sendPhoto?chat_id={chat_id}')
+    else:
+        requests.post(f'{url}sendPhoto?chat_id={chat_id}', files=files)
+
+
+def upload_file(chat_id, file):
+    files = {
+        'document': open(file, 'rb')
+    }
+    if safe_mode:
+        print(f'{url}sendDocument?chat_id={chat_id}')
+    else:
+        requests.post(f'{url}sendDocument?chat_id={chat_id}', files=files)
+
+
+def upload_video(chat_id, file, caption=''):
+    files = {
+        'video': open(file, 'rb')
+    }
+    if safe_mode:
+        print('{url}sendVideo?chat_id={chat_id}&caption={caption}')
+    else:
+        requests.post(f'{url}sendVideo?chat_id={chat_id}&caption={caption}', files=files)
+
+
+def append_log(user, msg):
+    with open(f'{path}log.txt', 'a', encoding='cp1251') as f:
+        try:
+            f.write(f'[{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] {user}: {msg}' + '\n')
+        except Exception as e:
+            f.write(f'Exception {e}' + '\n')
+
+
+def get_admins() -> list:
+    with open(f'{path}names.json', 'r') as f:
+        _ids = json.load(f)
+        result = []
+        for userid in _ids:
+            if _ids[userid]['is_admin']:
+                result.append(userid)
+    return result
