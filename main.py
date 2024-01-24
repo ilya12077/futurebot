867bb6cbd3b5e831766fafc532ddd358d9b3cd5a
ilya12077 <mr.ilya.1207@gmail.com>
2024-01-24 17:17:14 +0300

    pc port change

--- main.py
+++ main.py
@@ -342,5 +342,5 @@ if __name__ == '__main__':
     if os.environ.get('AM_I_IN_A_DOCKER_CONTAINER', False):
         serve(app, host='0.0.0.0', port=8881, url_scheme='http')
     else:
-        app.run(host='192.168.1.10', port=8881)
+        app.run(host='192.168.1.10', port=8885)
         # app.run(host='192.168.1.21', port=8881)
