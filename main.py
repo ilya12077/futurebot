1d04f29d8858e8443ea73598760afacee98cf703
ilya12077 <52870065+ilya12077@users.noreply.github.com>
2024-08-02 20:59:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ def firewall():
     if request.method == "GET":
         return 'I\'m working'
     r = request.get_json()
-    # with open(f'{path}data/quires.txt', 'a', encoding='utf-8') as f:
-    #    f.write(str(r) + '\n')
+    with open(f'{path}data/quires.txt', 'a', encoding='utf-8') as f:
+       f.write(str(r) + '\n')
     print(r)
     current_time = int(time.time())
     if current_time - pendingupdates_lastchecked > 60:
@@ -37,7 +37,7 @@ def firewall():
         response = requests.get(f'{tools.url}getWebhookInfo')
         if response.status_code == 200:
             pendingupdates_count = response.json().get("result", {}).get("pending_update_count", 0)
-            if pendingupdates_count > 25:
+            if pendingupdates_count > 250:
                 if current_time - pendingupdates_lastsent > 60 * 5:  # 3600 секунд = 1 час
                     tools.send_message(647372660, f'⭕Я заметил, что pending updates сейчас: <b>{pendingupdates_count}</b>\n{tools.url}getWebhookInfo')
                     pendingupdates_lastsent = current_time
