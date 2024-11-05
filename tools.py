bab4de4bc803555459d024f502f7e08774cfb9ea
ilya12077 <52870065+ilya12077@users.noreply.github.com>
2024-11-05 18:41:35 +0300

    Update tools.py

--- tools.py
+++ tools.py
@@ -63,7 +63,7 @@ def asked_usrids(action, user_id, username, reply_to_message_id: int | None, mes
     elif action == 'add' and switch_entire_authorization:
         if not switch_safe_mode:
             restrictChatMember_msgSend(chat_id=future_group_id, user_id=user_id)
-            r = send_message(future_group_id, f'{username}, добро пожаловать в чатик! Нажимайте кнопку ниже, только если вы человек. Иначе вы не сможете писать в чат', {'inline_keyboard': [[{'text': 'Подтверждаю', 'callback_data': user_id}]]}, reply_to_message_id=reply_to_message_id, message_thread_id=message_thread_id)
+            r = send_message(future_group_id, f'{username}, добро пожаловать в чатик! Нажимайте кнопку ниже, только если Вы человек. Иначе Вы не сможете писать в чат', {'inline_keyboard': [[{'text': 'Подтверждаю', 'callback_data': user_id}]]}, reply_to_message_id=reply_to_message_id, message_thread_id=message_thread_id)
             if r is not None:
                 # print(r.json())
                 wait_for_deletion(r.json()['result']['message_id'], authentication_message_timeout)
