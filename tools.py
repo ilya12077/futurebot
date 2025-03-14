fc269da44500576cd04a199f18a6011fb80514b7
ilya12077 <52870065+ilya12077@users.noreply.github.com>
2025-03-14 21:56:14 +0300

    Update tools.py

--- tools.py
+++ tools.py
@@ -16,9 +16,9 @@ switch_entire_authorization = True  # T авторизация (отправка
 switch_message_deletion = True  # T любое удаление сообщение
 switch_forward_deletion = True  # T пересылка соо в группу
 
-spam_timeout = 2 * 60  # в секундах
+spam_timeout = 60 * 60 * 3  # в секундах
 authentication_message_timeout = 60 * 1
-max_duplicate_messages = 3
+max_duplicate_messages = 2
 max_retries = 5
 
 load_dotenv(find_dotenv())
