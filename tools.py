460cb3a0dac34b3f29bb5150d2d5b619ebf1780c
ilya12077 <52870065+ilya12077@users.noreply.github.com>
2025-03-21 12:49:37 +0300

    ban 10h

--- tools.py
+++ tools.py
@@ -305,7 +305,7 @@ def get_admins() -> list:
     return result
 
 
-def restrictChatMember_msgSend(chat_id: int | str, user_id: int | str, duration: int = 60 * 60 * 6):
+def restrictChatMember_msgSend(chat_id: int | str, user_id: int | str, duration: int = 60 * 60 * 10):
     until_date = int(time.time()) + duration
     send_body = {
         'chat_id': chat_id,
