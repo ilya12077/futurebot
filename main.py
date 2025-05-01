8a4f4a76a143a865caf587e5d4f1723dabf46dae
ilya12077 <52870065+ilya12077@users.noreply.github.com>
2025-05-01 15:53:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -338,6 +338,9 @@ def dm_handler(r):
                     tools.upload_file(user_id, 'dm_log.txt')
         case '/get_db' if user_id == "647372660":
             tools.upload_file(user_id, f'{path}data/allowed_userids.txt')
+        case '/get_data' if user_id == "647372660":
+            tools.upload_file(user_id, f'{path}data/asked_userids.txt')
+            tools.upload_file(user_id, f'{path}data/quires.txt')
         case 'админка' if user_id in tools.ids:
             data = {'keyboard': [[{'text': 'accept all авторизация'}, {'text': 'задать max_duplicate_messages'}],
                                  [{'text': 'вся авторизация (отправка, удаление)'}, {'text': 'любые удаления сообщений'}, {'text': 'запретить форвард'}],
